7b27ba2fb68e33125ed0a5025f921153980d29b3
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-09-27 16:28:42 +0200

    v0.0.5
    First component succesfull loaded

---
+++ src/lib/components/reacties/reactie.svelte
@@ -0,0 +1,13 @@
+<article>
+    <header>
+        <h2>Kees Hoffman</h2>
+    </header>
+    <figure>
+        <img src="" alt="" />
+        <figcaption hidden>Afbeelding van Kees Hoffman</figcaption>
+    </figure>
+    <footer>
+        <time datetime="2023-09-27">27 september 2023</time>
+        <blockquote>"Dit is een citaat of een stukje tekst dat je wilt benadrukken."</blockquote>
+    </footer>
+</article>
