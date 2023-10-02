7e9d7f5709f8be5d6b9f9ba3a21f0ab17f181c8c
RayanSp <rayan.spall@hva.nl>
2023-10-02 16:38:37 +0200

    Personen pagina
    Start gemaakt voor de personen pagina

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,15 @@
     import Navbar from '$lib/components/navigatiebalk/navigatie.svelte';
     import Reactie from '$lib/components/reacties/reactie.svelte';
     import Searchbar from '$lib/components/reacties/searchbar/searchbar.svelte';
+    import Personen from '$lib/components/personen/persoon.svelte';
 </script>
 
 <Searchbar />
 
 <Navbar />
 
-<Reactie />
+<!-- <Reactie /> -->
+
+<Personen />
 
 
