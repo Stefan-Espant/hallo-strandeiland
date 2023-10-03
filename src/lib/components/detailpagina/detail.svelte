dcaf13cc05d66370b5e8023433d07b4b9f0eef79
modous <mohamed1010@live.nl>
2023-10-03 15:02:05 +0200

    beginnetje detailpagina

---
+++ src/lib/components/detailpagina/detail.svelte
@@ -0,0 +1,54 @@
+<section>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <p class="type">Wens</p>
+    <h1 class="titel">Hondenstrand</h1>
+
+    <p class="datum">Geplaatst op 25 mei 2024</p>
+   
+
+    <p>Hier komt alle info over de wens</p>
+
+    <article>
+        <button class="knop">Ik deel deze wens</button>
+        <button class="knop">Ik wil helpen</button>
+        <button class="knop">Ik wil trekker zijn</button>
+    </article>
+    
+    <p>Deel dit: <a href=""><i style="font-size:24px" class="fa">&#xf08c;</i></a> 
+                 <a href=""><i style="font-size:24px" class="fa">&#xf09a;</i></a></p>
+</section>
+
+
+<style>
+    section{
+        margin: 30px;
+    }
+
+    .type{
+       
+    }
+    .titel{
+        font-size: 30px;
+        margin: 30px;
+    }
+
+    .datum{
+        margin-left: 30px;
+        padding: 20px;
+    }
+
+    article{
+        
+        padding-top: 50px;
+    }
+    button{
+        background-color: black;
+        color: white;
+        margin: 5px;
+    }
+    .knop{
+        padding: 10px;
+    }
+    
+</style>
