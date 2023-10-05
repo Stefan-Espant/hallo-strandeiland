c3a260260123bfa9e9fb1b4e9d74f01cc2feb822
David van Boxtel <davidvb00@gmail.com>
2023-10-05 20:59:03 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <script>
     import Navbar from '$lib/components/navigatiebalk/navigatie.svelte';
-    import Reactie from '$lib/components/reacties/reactie.svelte';
+    // import Reactie from '$lib/components/reacties/reactie.svelte';
     import Searchbar from '$lib/components/reacties/searchbar/searchbar.svelte';
+    import Homepage from '$lib/components/homepage/homepage.svelte';
 </script>
 
 <Searchbar />
 
 <Navbar />
 
-<Reactie />
+<Homepage />
+
+<!-- <Reactie /> -->
 
 
