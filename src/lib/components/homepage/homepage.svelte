c4fa7965859b33d2dedd82d2b1aba568ff618df8
David van Boxtel <davidvb00@gmail.com>
2023-10-05 22:04:21 +0200

    update

---
+++ src/lib/components/homepage/homepage.svelte
@@ -0,0 +1,253 @@
+<main>
+	<section>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+		<article class="card__article">
+			<div class="card__image">
+				<img
+					src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+					alt="image"
+					class="card__img"
+				/>
+			</div>
+
+			<div class="card__data">
+				<h2 class="card__name">Strand</h2>
+				<p class="card__description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic t
+				</p>
+
+				<a href="#" class="button">Lees meer</a>
+			</div>
+		</article>
+	</section>
+</main>
+
+<!-- <section>
+	<img
+		src="https://hips.hearstapps.com/hmg-prod/images/view-of-beach-against-clear-blue-sky-royalty-free-image-1614177716.?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+		alt="foto van "
+	/>
+	<h2>Waterleiding</h2>
+	<time>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="icon icon-tabler icon-tabler-flag-2"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			stroke-width="2"
+			stroke="currentColor"
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M5 14h14v-9h-14v16" />
+		</svg>
+		Geplaatst op: 23-02-2000</time
+	>
+	<p />
+</section> -->
+
+<style>
+	/*=============== CARD ===============*/
+	section {
+		background-color: lightgrey;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		flex-direction: row;
+		width: 100%;
+		gap: 3em;
+	}
+
+	.card__article {
+		width: 300px; /* Remove after adding swiper js */
+		border-radius: 1.25rem;
+		overflow: hidden;
+	}
+
+	.card__image {
+		position: relative;
+		margin-bottom: -0.75rem;
+	}
+
+	.card__data {
+		background-color: #fff;
+		padding: 1.5rem 2rem;
+		border-radius: 1rem;
+		text-align: center;
+		position: relative;
+		z-index: 10;
+	}
+
+	.card__img {
+		width: 100%;
+		margin: 0 auto;
+		height: 100%;
+		position: relative;
+		z-index: 5;
+	}
+
+	.card__description {
+		font-weight: 500;
+		margin-bottom: 1.75rem;
+	}
+
+	.button {
+		border: none;
+		font-size: 16px;
+		padding: 8px 16px;
+		background-color: yellow;
+		border-radius: 6px;
+		margin: 14px;
+	}
+</style>
