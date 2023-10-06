d169c8e8b165b76052e05a71bd75f988896edd22
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-10-06 11:38:02 +0200

    v0.3.2
    removed personen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import Navbar from '$lib/components/navigatiebalk/navigatie.svelte';
     import Reactie from '$lib/components/reacties/reactie.svelte';
     import Searchbar from '$lib/components/searchbar/searchbar.svelte';
-    import Personen from '$lib/components/personen/persoon.svelte';
 
 	export let data;
 	console.log(data);
@@ -73,8 +72,6 @@
 	</section>
 
     <Reactie />
-
-    <Personen /> 
 </main>
 
 <style>
