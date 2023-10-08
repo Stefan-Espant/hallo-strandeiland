265c70b40cd155e865a5b8be78bc729564a09c19
modous <mohamed1010@live.nl>
2023-10-08 18:16:49 +0200

    .

--- src/routes/wens/[id]/+page.svelte
+++ src/routes/wens/[id]/+page.svelte
@@ -32,15 +32,16 @@
 
 
 <section>
-<article>
-    <button class="knop">Ik deel deze wens</button>
-    <button class="knop">Ik wil helpen</button>
-    <button class="knop">Ik wil trekker zijn</button>
-</article>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <article>
+            <button class="knop">Ik deel deze wens</button>
+            <button class="knop">Ik wil helpen</button>
+            <button class="knop">Ik wil trekker zijn</button>
+        </article>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<p>Deel dit: <a href=""><i style="font-size:24px" class="fa">&#xf08c;</i></a> 
-             <a href=""><i style="font-size:24px" class="fa">&#xf09a;</i></a></p>
+    <p>Deel dit: <a href=""><i style="font-size:24px" class="fa">&#xf08c;</i></a> 
+                 <a href=""><i style="font-size:24px" class="fa">&#xf09a;</i></a>
+                </p>
 </section>
 
 
